b7ee7806d67691c10361ab1af590cc45b4936d7c
rodims-code <ridrodims@hotmail.com>
2024-12-05 15:17:59 +0100

    Cration du model et products

--- store/models.py
+++ store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Produit 
+- Nom
+- Prix 
+- Quantite en stoks
+- Discription
+- Image
+"""
+
+class product(models.Model) :
+    # le champ pour le nom, c'est la  longuer du nom
+    name = models.CharField(max_length=128)
+    # Pour le prix de l'article
+    price = models.FloatField(default=0.0)
+    # Pour la quantite en stok
+    stok = models.FloatField(default=0)
+    # pourquoi textfield ? Juste par ce qu'il prend plus de carataire que le charfield
+    description = models.TextField(blank=True)
+    #Pour cree une image
+    image = models.ImageField(upload_to = 'Products', blank=True, null=True)
