a48285bfd15e25cd64b8896ac42eade8d6ee329d
rodims-code <ridrodims@hotmail.com>
2024-12-12 04:30:11 +0100

    Afficher la page de detail d'un produit

--- store/models.py
+++ store/models.py
@@ -12,15 +12,15 @@ Produit
 """
 
 class Product(models.Model) :
-    # le champ pour le nom, c'est la  longuer du nom
+
     name = models.CharField(max_length=128)
-    # Pour le prix de l'article
+    slug = models.SlugField(max_length=128)
     price = models.FloatField(default=0.0)
-    # Pour la quantite en stok
+
     stok = models.FloatField(default=0)
-    # pourquoi textfield ? Juste par ce qu'il prend plus de carataire que le charfield
+
     description = models.TextField(blank=True)
-    #Pour cree une image
+
     image = models.ImageField(upload_to = 'products', blank=True, null=True)
 
     def __str__(self):
