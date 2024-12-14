40c606734923f11beb7f7a9d61fb5c1d5df54dd8
rodims-code <ridrodims@hotmail.com>
2024-12-14 02:13:39 +0100

    Ajout du formulaire d'inscription

--- shop/urls.py
+++ shop/urls.py
@@ -18,11 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from store.views import index, product_detail
+from accounts.views import singup
 
-from  shop import settings
+from shop import settings
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('singup/', singup, name="singup"),
     path('product/<str:slug>/', product_detail, name = "product"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
