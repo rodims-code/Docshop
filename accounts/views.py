f56fbd5fc64830b1aba26176b765afa818eaf55b
rodims-code <ridrodims@hotmail.com>
2024-12-15 23:15:01 +0100

    Creation du formulaire et creation d'un compte client

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+User = get_user_model()
+
 def singup(request) :
+    if request.method == "POST" :
+        # Traiter le formulaire
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username,
+                                        password=password)
+        login(request, user)
+        return redirect('index')
     return render(request, 'accounts/singup.html')
