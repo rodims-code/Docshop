5092e496694ac1f50a44925ed9cb7893cd1e3d6f
rodims-code <ridrodims@hotmail.com>
2024-12-18 03:03:35 +0100

    Creer le models Order

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from shop.settings import AUTH_USER_MODEL
+
 # Create your models here.
 
 """
@@ -37,6 +39,14 @@ class Product(models.Model) :
 - Produits 
 - Commander ou non
 """
+class Order(models.Model) :
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+
+    def __str__ (self) :
+        return f"{self.product.name} ({self.quantity})"
 
 # Panier (Card)
 """
