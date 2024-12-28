7336a8c47183b27c58a683aa78bb3d25f28079b0
rodims-code <ridrodims@hotmail.com>
2024-12-28 15:44:03 +0100

    Ajouter un article dans le panier

--- shop/urls.py
+++ shop/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from store.views import index, product_detail
+from store.views import index, product_detail, add_to_cart
 from accounts.views import singup, logout_user, login_user
 
 from shop import settings
@@ -29,4 +29,5 @@ urlpatterns = [
     path('login/', login_user, name="login"),
     path('logout/', logout_user, name="logout"),
     path('product/<str:slug>/', product_detail, name = "product"),
+    path('product/<str:slug>/add_to_cart/', add_to_cart, name = "add_to_cart"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
+
 from django.http import HttpResponse
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
-from store.models import Product
+from store.models import Product, Carts, Order
+
 
 # Create your views here.
 
@@ -14,3 +16,18 @@ def index (request):
 def product_detail (request, slug) :
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'store/detail.html', context={"product" : product})
+
+def add_to_cart (request, slug) :
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    cart, _ = Carts.objects.get_or_create(user=user)
+    order, created = Order.objects.get_or_create(user=user,
+                                                 product=product)
+    if created :
+        cart.order.add(order)
+        cart.save()
+    else:
+        order.quantity += 1
+        order.save()
+
+    return redirect(reverse("product", kwargs={"slug":slug}))
